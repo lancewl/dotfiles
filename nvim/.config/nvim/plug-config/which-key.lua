61a7332fcf06237c29fb16564145f2e74f762ee9
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-10-19 22:10:37 -0700

    Nvim - Update which-key settings

--- nvim/.config/nvim/plug-config/which-key.lua
+++ nvim/.config/nvim/plug-config/which-key.lua
@@ -7,6 +7,12 @@ wk.setup{
     ["<cr>"] = "RET",
     ["<tab>"] = "TAB",
   },
+  window = {
+    border = "single", -- none, single, double, shadow
+    position = "bottom", -- bottom, top
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+  },
   layout = {
     spacing = 8, -- spacing between columns
   },
