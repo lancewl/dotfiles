632b6b6d58b763feab6241e8267abb8481682c82
Lance Weng Lin <lancewenglin@gmail.com>
2022-03-14 18:30:35 -0700

    Nvim - Refactor treesitter config to lua
    update

--- nvim/.config/nvim/plug-config/treesitter.lua
+++ nvim/.config/nvim/plug-config/treesitter.lua
@@ -1,4 +1,3 @@
-lua <<EOF
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
@@ -15,10 +14,9 @@ require'nvim-treesitter.configs'.setup {
     "yaml",
     "html",
     "lua",
-    "vim"
+    "vim",
   },
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-EOF
