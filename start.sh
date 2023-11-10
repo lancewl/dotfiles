7f8196f8c048c2dc79e7aad466c20dd7f4594672
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2023-11-10 15:06:08 -0800

    feat: add start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+# Install brew dependincies
+brew bundle install --file=Brewfile
+
+# Setup oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# install colorls
+sudo gem install colorls
