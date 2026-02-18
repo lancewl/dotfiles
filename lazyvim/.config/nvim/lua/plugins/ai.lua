416bfd745034f0c52e550c8befedcd0d424b9606
Lance Weng Lin <lance.wenglin@mongodb.com>
2026-02-18 14:35:09 -0800

    feat(nvim): migrate from copilot chat to codecompanion

--- lazyvim/.config/nvim/lua/plugins/ai.lua
+++ lazyvim/.config/nvim/lua/plugins/ai.lua
@@ -1,30 +1,27 @@
 return {
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    opts = function()
-      local user = vim.env.USER or "User"
-      user = user:sub(1, 1):upper() .. user:sub(2)
-      return {
-        auto_insert_mode = false,
-        headers = {
-          user = "   " .. user .. " ",
-          assistant = "   Copilot ",
-          tool = "   Tool: ",
-        },
-        window = {
-          layout = "float",
-          width = 0.4,
-          height = 0.5,
-          border = "rounded", -- 'single', 'double', 'rounded', 'solid'
-          title = "  AI Assistant",
-          zindex = 100, -- Ensure window stays on top
-        },
-      }
-    end,
-  },
-  {
     "olimorris/codecompanion.nvim",
     version = "^18.0.0",
+    lazy = false,
+    config = function()
+      require("codecompanion").setup()
+      vim.cmd([[cab cc CodeCompanion]])
+    end,
+    keys = {
+      { "<leader>a", "", desc = "+AI", mode = { "n", "x" } },
+      {
+        "<leader>ac",
+        "<cmd>CodeCompanionChat Toggle<cr>",
+        mode = { "n", "x" },
+        desc = "Chat (CodeCompanion)",
+      },
+      {
+        "<leader>aa",
+        "<cmd>CodeCompanionActions<cr>",
+        mode = { "n", "x" },
+        desc = "Actions (CodeCompanion)",
+      },
+    },
     opts = {},
     dependencies = {
       "nvim-lua/plenary.nvim",
