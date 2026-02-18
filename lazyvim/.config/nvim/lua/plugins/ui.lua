5be10e11f5644da11774cdde95ee263669f3ed7e
Lance Weng Lin <lance.wenglin@mongodb.com>
2026-02-17 18:00:41 -0800

    fix(nvim): ui updates

--- lazyvim/.config/nvim/lua/plugins/ui.lua
+++ lazyvim/.config/nvim/lua/plugins/ui.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       options = {
         theme = "auto",
-        section_separators = { "", "" },
+        section_separators = { left = "", right = "" },
         component_separators = { "", "" },
       },
       sections = {
@@ -12,8 +12,10 @@ return {
           {
             "mode",
             fmt = function(str)
-              return str:sub(1, 1)
+              return " " .. str
             end,
+            separator = { left = "", right = "" },
+            right_padding = 2,
           },
         },
         lualine_b = {
@@ -28,67 +30,44 @@ return {
             end,
           },
         },
+        lualine_z = {
+          {
+            function()
+              return " " .. os.date("%R")
+            end,
+            separator = { left = "", right = "" },
+            left_padding = 2,
+          },
+        },
       },
     },
   },
   {
-    "nvimdev/dashboard-nvim",
-    opts = function()
-      local logo = [[
-      ██╗      █████╗ ███╗   ██╗ ██████╗███████╗██╗    ██╗██╗     
-      ██║     ██╔══██╗████╗  ██║██╔════╝██╔════╝██║    ██║██║     
-      ██║     ███████║██╔██╗ ██║██║     █████╗  ██║ █╗ ██║██║     
-      ██║     ██╔══██║██║╚██╗██║██║     ██╔══╝  ██║███╗██║██║     
-      ███████╗██║  ██║██║ ╚████║╚██████╗███████╗╚███╔███╔╝███████╗
-      ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚══════╝ ╚══╝╚══╝ ╚══════╝
-      ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      local opts = {
-        theme = "doom",
-        hide = {
-          -- this is taken care of by lualine
-          -- enabling this messes up the actual laststatus setting after loading a file
-          statusline = false,
-        },
-        config = {
-          header = vim.split(logo, "\n"),
-          -- stylua: ignore
-          center = {
-            { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-            { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-            { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-            { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-            { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
-            { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-            { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
-            { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-            { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
-          },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
           end,
+          header = [[
+        ██╗      █████╗ ███╗   ██╗ ██████╗███████╗██╗    ██╗██╗     
+        ██║     ██╔══██╗████╗  ██║██╔════╝██╔════╝██║    ██║██║     
+        ██║     ███████║██╔██╗ ██║██║     █████╗  ██║ █╗ ██║██║     
+        ██║     ██╔══██║██║╚██╗██║██║     ██╔══╝  ██║███╗██║██║     
+        ███████╗██║  ██║██║ ╚████║╚██████╗███████╗╚███╔███╔╝███████╗
+        ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚══════╝ ╚══╝╚══╝ ╚══════╝
+ ]],
         },
-      }
-
-      for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
-      end
-
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "DashboardLoaded",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      return opts
-    end,
+      },
+    },
   },
 }
